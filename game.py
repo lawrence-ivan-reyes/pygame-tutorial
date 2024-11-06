073e9ff13cada85e0f1e0dcf0f6091c2193a5492
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-11-06 18:42:23 -0500

    revised GameObject class to account for image, rendered first image to screen

--- game.py
+++ game.py
@@ -1,23 +1,19 @@
 import pygame
 pygame.init()
 
+screen = pygame.display.set_mode((500, 500))
+
 class GameObject(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, image):
         super(GameObject, self).__init__()
-        self.surf = pygame.Surface((width, height))
-        self.surf.fill((255, 0, 255))  
+        self.surf = pygame.image.load(image)
         self.x = x
         self.y = y
 
     def render(self, screen):
         screen.blit(self.surf, (self.x, self.y))
 
-box = GameObject(120, 300, 50, 50)  
-
-screen = pygame.display.set_mode((500, 500))
-
-surf = pygame.Surface((50, 50))  
-surf.fill((255, 111, 33))  
+apple = GameObject(120, 300, './images/apple.png')
 
 running = True
 while running:
@@ -27,10 +23,8 @@ while running:
 
     screen.fill((255, 255, 255))
     
-    pygame.display.flip()
-
-    screen.fill((255, 255, 255)) 
-    box.render(screen)           
+    apple.render(screen)
+     
     pygame.display.flip()    
 
 pygame.quit()
