b624994fa75d1a722ebdcacaf5462052ea740f78
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-11-06 19:25:13 -0500

    fixed Apple subclass & methods

--- game.py
+++ game.py
@@ -18,12 +18,13 @@ class GameObject(pygame.sprite.Sprite):
 
 class Apple(GameObject):
     def __init__(self):
-        x = randint(50, 400)
-        super(Apple, self).__init__(x, 0, './images/apple.png')
+        super(Apple, self).__init__(0, 0, './images/apple.png')
+        self.dx = 0
         self.dy = (randint(0, 200) / 100) + 1
+        self.reset()
 
     def move(self):
-        # self.x += self.dx
+        self.x += self.dx
         self.y += self.dy
         if self.y > 500: 
             self.reset()
